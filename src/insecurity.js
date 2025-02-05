f3c5498cd1f66f0814184da8c2e4b4cde151120c
pushpa-u <pushpa.u@10decoders.in>
2025-02-05 12:37:38 +0530

    Resolved critical and high error

--- src/insecurity.js
+++ src/insecurity.js
@@ -1,7 +1,7 @@
 //Genertae HIGH error
 
-export const isAuthorized = () => expressJwt(({ secret: publicKey }) )
-export const denyAll = () => expressJwt({ secret: '' + Math.random() } )
-export const authorize = (user = {}) => jwt.sign(user, privateKey, { expiresIn: '6h', algorithm: 'RS256' })
+// export const isAuthorized = () => expressJwt(({ secret: publicKey }) )
+// export const denyAll = () => expressJwt({ secret: '' + Math.random() } )
+// export const authorize = (user = {}) => jwt.sign(user, privateKey, { expiresIn: '6h', algorithm: 'RS256' })
 // export const verify = (token: string) => token ? (jws.verify as ((token: string, secret: string) => boolean))(token, publicKey) : false
 // export const decode = (token: string) => { return jws.decode(token)?.payload }
